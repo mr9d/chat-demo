8ee3e58587a3633b6b5867a3f3a260cc2a3b6b86
Aleksandr Kozlov <kaa_12@mail.ru>
2020-04-06 23:07:49 +0400

    Update cors headers

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use(express.json());
 
 app.post('/msg', async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods','POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   messenger.addMessage(req.body.name, req.body.message);
   res.json('TBD');
 });
 
 app.get('/msg', async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods','GET');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   if(req.query.since) {
     res.json(messenger.getLastMessagesSince(req.query.since));
   } else {
